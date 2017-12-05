8065b644f35e82571bd31809fbe8ca2975f47836
Mark Andres <mandres12@gmail.com>
2017-12-05 10:33:52 -0600

    Setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import moment from 'moment'
+import numeral from 'numeral'
 
 const ExpenseListItem = ({ id, description, amount, createdAt, note }) => (
     <div>
         <h3><NavLink to={`/edit/${id}`}>{description}</NavLink></h3>
-        <p>{`\$${amount / 100} - ${note} - ${moment(createdAt).format("dddd, MMMM Do YYYY")}`}</p>
+        <p>
+            {numeral(amount / 100).format('$0,0.00')} 
+            - 
+            {note} 
+            - 
+            {moment(createdAt).format("dddd, MMMM Do YYYY")}}
+        </p>
     </div> 
 )
 
--- src/tests/components/ExpenseListItem.test.js
+++ src/tests/components/ExpenseListItem.test.js
@@ -4,6 +4,6 @@ import ExpenseListItem from '../../components/ExpenseListItem'
 import expenses from '../fixtures/expenses'
 
 test('should render ExpenseListItem with an expense', () => {
-    const wrapper = shallow( <ExpenseListItem { ...expenses[0] } key={ expenses[0].id } />)
+    const wrapper = shallow( <ExpenseListItem { ...expenses[2] } key={ expenses[2].id } />)
     expect(wrapper).toMatchSnapshot()
 })
