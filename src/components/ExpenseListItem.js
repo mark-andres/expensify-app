bbfcf40e6aae96c9a32c8c3698672d859c0bace5
Mark Andres <mandres12@gmail.com>
2017-12-19 16:35:59 -0600

    Add Expensify styles

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,19 +1,16 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import moment from 'moment'
 import numeral from 'numeral'
 
 const ExpenseListItem = ({ id, description, amount, createdAt, note }) => (
-    <div>
-        <h3><NavLink to={`/edit/${id}`}>{description}</NavLink></h3>
-        <p>
-            {numeral(amount / 100).format('$0,0.00')} 
-            - 
-            {note} 
-            - 
-            {moment(createdAt).format("dddd, MMMM Do YYYY")}
-        </p>
-    </div> 
+    <Link className="list-item" to={`/edit/${id}`}>
+        <div>
+            <h3 className="list-item__title">{description}</h3>
+            <span className="list-item__sub-title">{moment(createdAt).format("dddd, MMMM Do YYYY")}</span>
+        </div>
+        <h3 className="list-item__data">{numeral(amount / 100).format('$0,0.00')}</h3>
+    </Link>
 )
 
 export default ExpenseListItem
--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import selectExpensesTotal from '../selectors/expenses-total'
 import selectExpenses from '../selectors/expenses'
 import numeral from 'numeral'
 
-export const ExpensesSummary = (props) => (
-    <p>
-        Viewing {props.count} expenses totalling {numeral(props.total / 100).format('$0,0.00')}
-    </p>
-)
+export const ExpensesSummary = ({ expenseCount, expenseTotal }) => { 
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses'
+    const formattedExpensesTotal = numeral(expenseTotal / 100).format('$0,0.00')
+    
+    return (
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">
+                    Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span>
+                </h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
+        </div>
+    )
+ }
 
 const mapStateToProps = (state) => {
     const expenses = selectExpenses(state.expenses, state.filters)
     return {
-        count: expenses.length,
-        total: selectExpensesTotal(expenses)
+        expenseCount: expenses.length,
+        expenseTotal: selectExpensesTotal(expenses)
     }
 }
 
